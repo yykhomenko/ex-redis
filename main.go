cfd73f25e19312a4c4d08f0b1c87fdb74bb13cf9
yykhomenko <yykhomenko@gmail.com>
2020-11-29 21:42:54 +0200

    clear

--- main.go
+++ main.go
@@ -1,42 +1,5 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-	"log"
-
-	"github.com/mediocregopher/radix"
-)
-
 func main() {
-	c, err := radix.Dial("tcp", "127.0.0.1:6379")
-	if err != nil {
-		panic(err)
-	}
-	defer c.Close()
 
-	if err := test(c); err != nil {
-		log.Fatal(err)
-	}
-}
-
-func test(client radix.Conn) error {
-
-	key := "k"
-	val := "v"
-
-	if err := client.Do(radix.Cmd(nil, "SET", key, val)); err != nil {
-		return err
-	}
-
-	var out string
-	if err := client.Do(radix.Cmd(&out, "GET", key)); err != nil {
-		return err
-	} else if out != val {
-		return errors.New("got wrong value")
-	}
-
-	fmt.Println(out)
-
-	return nil
 }
